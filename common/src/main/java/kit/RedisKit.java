1187116508c446e8da3d1b5e9af26f468d3931a0
jiangxiaowei <jiangxiaowei@zxisl.com>
2018-08-03 17:13:24 +0800

    ..

---
+++ common/src/main/java/kit/RedisKit.java
@@ -0,0 +1,16 @@
+package kit;
+
+public class RedisKit {
+
+    public void RedisKit(String host, int port) {
+
+    }
+
+    public void RedisKit(String host, int port, String password) {
+
+    }
+
+    private void initJedis(String host, int port, String password) {
+
+    }
+}
